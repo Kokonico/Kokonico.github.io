5db10690a30115990b8a20c8d904e5160467d1ef
Kokonico <kokonico@duck.com>
2024-07-12 04:26:02 +0000

    added Hackclub Webring

--- footer.js
+++ footer.js
@@ -17,9 +17,24 @@ document.addEventListener('DOMContentLoaded', function() {
       <a href="https://replit.com/@Kokonico"><img src="socials/Replit_Logo_Symbol.svg" width="25px" height="25"></a>
       <a href="https://www.youtube.com/channel/UC1m2DPc09SvbbNe1reTaZbw"><img src="socials/ytlogo.png" width="25px" height="25px"></a>
       <a href="https://gitlab.com/Kokonico" ><img src="socials/gitlab.svg" width="25px" height="25px"></a>
+      <div id="webring-wrapper">
+        <a href="https://webring.hackclub.com/" id="previousBtn" class="webring-anchor" title="Previous">‹</a>
+        <a href="https://webring.hackclub.com/" class="webring-logo" title="Hack Club Webring" alt="Hack Club Webring"></a>
+        <a href="https://webring.hackclub.com/" id="nextBtn" class="webring-anchor" title="Next">›</a>
+      </div>
       </p>
     </section>
   `;
 
+  function injectWebringScript() {
+    const webringContainer = document.getElementById('webring-wrapper');
+    const webringScript = document.createElement('script');
+    webringScript.src = 'https://webring.hackclub.com/embed.min.js';
+    webringContainer.appendChild(webringScript); 
+  }
+
+  
+
   document.body.insertAdjacentHTML('beforeend', footerHTML);
+  injectWebringScript();
 });
