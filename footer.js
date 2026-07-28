b9a3983a50e806f267f6c1d97930754c53c38ce9
Kokonico <kokonico@duck.com>
2026-07-28 01:18:10 -0700

    remove replit (rest in peace they're an AI platform now :<)

--- footer.js
+++ footer.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
   var footerHTML = `
     <hr>
     <section class="copyright" style="text-align: center;">
-      <p>© 2026 KKNC web wear</p>
+      <p>© 2026 KKNC</p>
       <p>
         <a href=index.html>Home</a>
         |
@@ -16,7 +16,6 @@ document.addEventListener('DOMContentLoaded', function() {
     <section class="socials" style="text-align: center;">
       <p>
           <a href="https://github.com/Kokonico"><img src="socials/github-mark-white.svg" width="25px" height="25px" alt="github"></a>
-          <a href="https://replit.com/@Kokonico"><img src="socials/Replit_Logo_Symbol.svg" width="25px" height="25" alt="replit"></a>
           <a href="https://www.youtube.com/channel/UC1m2DPc09SvbbNe1reTaZbw"><img src="socials/ytlogo.png" width="25px" height="25px" alt="youtube"></a>
           <a href="https://gitlab.com/Kokonico" ><img src="socials/gitlab.svg" width="25px" height="25px" alt="gitlab"></a>
           <a href="https://codeberg.org/Kokonico"><img src="socials/Codeberg_Logo.svg" width="25px" height="25px" alt="codeberg"></a>
